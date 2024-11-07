44363d9121fb55fe840e56701dfbe3f6552410bb
ttessier1 <ttessier@swhistlesoft.com>
2024-11-07 14:00:58 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,9 @@
-<?php
-if ( file_exists ( "readme.yaml" ) ){
-	require_once "../spyc.php";
-}
-?>
+
 <h1>HTML5 Space Invaders Sample</h1>
 <p>This is an HTML5 representation of the game that was created in dot net. You can check the downloads section. Click on the screen before you try to play to give focus to the game.</p>
 <ul>
 <li>Use the left and right arrows to move the ship.</li>
 <li>Space bar fires.</li>
 </ul>
-<script type="text/javascript" src="https://www.swhistlesoft.com/demo/SpaceInvaders/js/spaceinvaders.js"></script>
+<script type="text/javascript" src="js/spaceinvaders.js"></script>
 <canvas id="canvs" width="900" height="450"></canvas>
